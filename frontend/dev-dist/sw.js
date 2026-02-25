8ed85cb1512b47f3eb870ee6ea94e90b99d48d3f
unddefined <669483446@qq.com>
2026-02-25 21:29:23 +0800

    refactor: 优化Cooking.vue和OrderView.vue组件的结构和样式

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-d93339a4'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.i73aun421p8"
+    "revision": "0.uk5efjfjlcg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
